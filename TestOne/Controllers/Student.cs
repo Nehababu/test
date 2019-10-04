18030bb64171447f4733249799c5a276c4cebf42
ankita <ankitakumari650@gmail.com>
2019-10-04 22:53:48 +0530

    bvn

--- TestOne/Controllers/Student.cs
+++ TestOne/Controllers/Student.cs
@@ -12,5 +12,6 @@ namespace TestOne.Controllers
         public string Lstname { get; set; }
         public string Branch { get; set; }
         public string ght { get; set; }
+        public string hgfh { get; set; }
     }
 }
