1f0802bfc1ba0c685dc1edc84279a6fa0c73c037
ankita <ankitakumari650@gmail.com>
2019-10-04 22:42:17 +0530

    vb

--- TestOne/Controllers/Student.cs
+++ TestOne/Controllers/Student.cs
@@ -9,5 +9,8 @@ namespace TestOne.Controllers
     {
         public string Name { get; set; }
         public string City { get; set; }
+        public string Lstname { get; set; }
+        public string Branch { get; set; }
+        public string ght { get; set; }
     }
 }
